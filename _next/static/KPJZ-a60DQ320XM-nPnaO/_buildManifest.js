e6508138962bc6f6a9d68ef67bacf786fe9a8535
iagodevtech <iagodevtech@users.noreply.github.com>
2025-09-15 16:35:30 +0000

    deploy: 1b32720923be3753a34dc9377e7a1033b0b56051

---
+++ _next/static/KPJZ-a60DQ320XM-nPnaO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[{has:void 0,source:"/ecomify/api/stripe/webhook",destination:"/ecomify/api/webhooks/stripe"}],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-41b7ed050ee49bd2.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
